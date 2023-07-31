f0e5993f5d431636f61c2a52b8efd720adad7265
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-07-31 13:49:14 +0200

    lab 1&2

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -11,6 +11,22 @@ public class World {
         System.out.println();
         LinkedList<Direction> directions = convert(args);
         run(directions);
+        //----------------
+        System.out.println();
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+        //----------------
+        System.out.println();
+        MapDirection direction1 = MapDirection.NORTH;
+        System.out.println(direction1);
+        MapDirection direction2 = MapDirection.SOUTH;
+        System.out.println(direction2);
+        System.out.println(direction1.next());
+        System.out.println(direction2.next());
+        //----------------
         System.out.println("system zakończył działanie");
     }
 
